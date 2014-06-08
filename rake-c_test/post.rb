6d83ba9bc74ccdc8378b12867efe3c4d86acfc0e
chanshunli <chanshunli@gmail.com>
2014-06-08 08:36:08 +0800

    Commit 2014年 06月 08日 星期日 08:36:08 CST

---
+++ rake-c_test/post.rb
@@ -0,0 +1,22 @@
+class Post
+  ####skip_before_filter :verify_authenticity_token
+  ##222####before_filter :check_weixin_legality
+
+  def show
+    render :text => params[:echostr]  ### 验证URL有效性: echostr 随机字符串 
+  end
+
+  def create
+    if params[:xml][:MsgType] == "text"
+      render "echo", :formats => :xml
+    end
+  end
+
+  private
+  # 根据参数校验请求是否合法，如果非法返回错误页面
+  def check_weixin_legality
+    array = [Rails.configuration.weixin_token, params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  end
+end
+
